8d8dea848249d9c80721007f47304173b7284d71
winfred <achiengwinfred20@gmail.com>
2024-03-07 11:45:58 +0300

    gridview layout

--- app/src/main/java/com/winfred/volumecalculator/MainActivity.java
+++ app/src/main/java/com/winfred/volumecalculator/MainActivity.java
@@ -3,12 +3,17 @@ package com.winfred.volumecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView= findViewById(R.id.gridview);
     }
 }
